6ac84df90d8e89310e84e391a2e0f9db273f058f
Sittikorn Prasanlak <easy2dev@gmail.com>
2020-05-26 19:29:43 +0700

    initail commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+// $app->get('/', ['\App\Controllers\HomeController', 'home']);
+
+$app->run();
