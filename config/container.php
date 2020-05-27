d0e3c5bdfe9f2b2c24de1d405c7957517a101976
Sittikorn Prasanlak <easy2dev@gmail.com>
2020-05-27 20:46:32 +0700

    addition config

---
+++ config/container.php
@@ -0,0 +1,33 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use Selective\Config\Configuration;
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
+
+return [
+    // Add to container
+    Configuration::class => function () {
+        return new Configuration(require __DIR__ . '/settings.php');
+    },
+
+    App::class => function (ContainerInterface $container) {
+        AppFactory::setContainer($container);
+
+        return AppFactory::create();
+    },
+
+    ErrorMiddleware::class => function (ContainerInterface $container) {
+        $app = $container->get(App::class);
+        $settings = $container->get(Configuration::class)->getArray('error_handler_middleware');
+
+        return new ErrorMiddleware(
+            $app->getCallableResolver(),
+            $app->getResponseFactory(),
+            (bool)$settings['display_error_details'],
+            (bool)$settings['log_errors'],
+            (bool)$settings['log_error_details']
+        );
+    }
+];
--- public/index.php
+++ public/index.php
@@ -1,18 +1,3 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
-$app = AppFactory::create();
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
-
-// $app->get('/', ['\App\Controllers\HomeController', 'home']);
-
-$app->run();
+(require __DIR__ . '/../config/bootstrap.php')->run();
